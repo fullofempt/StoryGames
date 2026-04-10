e5720efd41e2d12afeb86adccced418d2a6c3f9d
fullofempt <131869779+fullofempt@users.noreply.github.com>
2026-04-10 17:22:08 +0500

    ADD: proj. page need fix footer

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,6 @@
 export const navItems = [
-  { label: 'О студии', href: '#about' },
-  { label: 'Наши проекты', href: '#projects' },
-  { label: 'Новости/девблог', href: '#news' },
-  { label: 'Контакты', href: '#contacts' },
+  { label: 'О студии', href: '/' },
+  { label: 'Наши проекты', href: '/projects' },
+  { label: 'Новости/девблог', href: '/news' },
+  { label: 'Контакты', href: '/contacts' },
 ];
---
+++ src/data/projects-page.ts
@@ -0,0 +1,28 @@
+export const projectRows = [
+    {
+        title: 'fractured-red',
+        description:
+            'Игра про лягушку, атмосферное приключение с ярким визуальным стилем, необычным миром и историей, которую хочется исследовать до конца.',
+        image: '/images/projects/fractured-red.png',
+        reverse: false,
+    },
+    {
+        title: 'another-destiny',
+        description:
+            'Нарративный проект с упором на персонажей, выборы и эмоциональную подачу. История развивается через атмосферу, музыку и визуальные детали.',
+        image: '/images/projects/another-destiny.png',
+        reverse: true,
+    },
+    {
+        title: 'fractured-red',
+        description:
+            'Проект с sci-fi настроением, сильной художественной частью и интригующим миром, где сюжет и исследование тесно связаны друг с другом.',
+        image: '/images/projects/fractured-red.png',
+        reverse: false,
+    },
+];
+
+export const featuredProject = {
+    title: 'another-destiny',
+    image: '/images/projects/another-destiny.png',
+};
